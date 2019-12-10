4691926b7fed15b7ece2ff0bcb2fc5afd0cd57a6
Pakulin Sergei <pakylin3@mail.ru>
2019-12-11 01:02:49 +0500

    реализация сортировки таблицы + стили

--- src/app/client/main/main.component.ts
+++ src/app/client/main/main.component.ts
@@ -1,8 +1,8 @@
-import { Component, ViewChild } from '@angular/core';
-import { MatPaginator, MatTableDataSource } from '@angular/material';
+import { AfterViewChecked, Component, ViewChild } from '@angular/core';
+import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { NgxPermissionsService } from 'ngx-permissions';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { DataService } from 'src/app/core/services/data/data.service';
 
@@ -14,39 +14,53 @@ import { DataService } from 'src/app/core/services/data/data.service';
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent {
+export class MainComponent implements AfterViewChecked {
 
   medicines$: Observable<Array<{}>>;
 
   displayedColumns: string[] = ['pharmacy', 'term', 'count'];
+  setMatSortFlag = new BehaviorSubject<boolean>(false);
 
   dataSource = new MatTableDataSource<any>();
 
   @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: false }) sort: MatSort;
 
   constructor(
     private ngxPermissions: NgxPermissionsService,
     private dataService: DataService,
     private database: AngularFireDatabase,
-  ) { 
+  ) {
     this.getAllMedicines();
   }
 
   getAllMedicines(): void {
     this.medicines$ = this.database.list('/medicines').valueChanges().pipe(
       tap(data => {
-        for (let i = 0; i < 15; i++) {
-          this.dataSource.data.push(data[0][0][i]);
+        for (const [index, pharmacy] of (data[0] as Array<[]>).entries()) { // Прибавляем название аптеки к каждому объекту
+          this.dataSource.data.push(pharmacy.map(object => {
+            return Object.assign(object, {
+              pharmacy: index+1
+            });
+          }));
         }
+        let concatArray = [];
+        for (const eachArr of this.dataSource.data) { // Сводим массивы всех аптек в один
+          concatArray = concatArray.concat(eachArr);
+        }
+        this.dataSource.data = concatArray;
         this.dataSource.paginator = this.paginator;
+        this.setMatSortFlag.next(true);
       })
     );
   }
 
-  private flatten(arr) {
-    return arr.reduce((flat, toFlatten) => {
-      return flat.concat(Array.isArray(toFlatten) ? this.flatten(toFlatten) : toFlatten);
-***REMOVED*****REMOVED*****REMOVED*** []);
+  ngAfterViewChecked(): void {
+    if (this.setMatSortFlag.getValue()) { // Для решения проблемы с matSort в***REMOVED***ngIf
+      this.dataSource.sort = this.sort;
+      this.setMatSortFlag.next(false);
+      this.setMatSortFlag.complete();
+    }
   }
 
 }
