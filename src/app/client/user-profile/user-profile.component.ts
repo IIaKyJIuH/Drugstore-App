319d787b3e7b464918c283bf91ed96d28268bee2
Pakulin Sergei <pakylin3@mail.ru>
2019-12-11 14:14:40 +0500

    initialized formData in user-profile

--- src/app/client/user-profile/user-profile.component.ts
+++ src/app/client/user-profile/user-profile.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthenticationService } from 'src/app/core/services/authentication/authentication.service';
+import { PasswordValidator } from 'src/app/core/services/registration/password-validator';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
-export class UserProfileComponent implements OnInit {
+export class UserProfileComponent {
 
-  constructor() { }
+***REMOVED*****REMOVED*****REMOVED****
+ ***REMOVED*****REMOVED*** Form for email changing
+ ***REMOVED*****REMOVED***/
+  emailChangeForm: FormGroup;
 
-  ngOnInit() {
-  }
+***REMOVED*****REMOVED*****REMOVED****
+ ***REMOVED*****REMOVED*** Form for changing user password.
+ ***REMOVED*****REMOVED***/
+  passwordChangeForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthenticationService
+  ) {
+    this.emailChangeForm = this.formBuilder.group({
+      email: ['', Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$') ] 
+    });
+    this.passwordChangeForm = this.formBuilder.group({
+      password: ['', Validators.compose([
+        Validators.minLength(6),
+        Validators.maxLength(12),
+      ])],
+      confirmPassword: ['', Validators.compose([
+        Validators.minLength(6),
+        Validators.maxLength(12),
+      ])],
+***REMOVED*****REMOVED*****REMOVED*** 
+    { validator: PasswordValidator.areEqual });
+   }
 
 }
