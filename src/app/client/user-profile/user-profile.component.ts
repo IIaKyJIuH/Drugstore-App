af8ea3ec1083710db22768e0d217de2fce013928
Pakulin Sergei <pakylin3@mail.ru>
2019-12-11 23:18:28 +0500

    изменение email пользователя

--- src/app/client/user-profile/user-profile.component.ts
+++ src/app/client/user-profile/user-profile.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { take } from 'rxjs/operators';
 import { AuthenticationService } from 'src/app/core/services/authentication/authentication.service';
+import { CredentialsModel } from 'src/app/core/services/models/credentials-model';
+import { NotificationService } from 'src/app/core/services/notification/notification.service';
 import { PasswordValidator } from 'src/app/core/services/registration/password-validator';
 
 @Component({
@@ -22,7 +25,8 @@ export class UserProfileComponent {
 
   constructor(
     private formBuilder: FormBuilder,
-    private authService: AuthenticationService
+    private authService: AuthenticationService,
+    private notifications: NotificationService
   ) {
     this.emailChangeForm = this.formBuilder.group({
       email: ['', Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$') ] 
@@ -38,7 +42,23 @@ export class UserProfileComponent {
       ])],
     }, 
     { validator: PasswordValidator.areEqual });
-   }
+  }
+
+  onEmailChangeSubmit(emailFormValues: Partial<CredentialsModel>): void {
+    const newEmail = emailFormValues.email;
+    if (newEmail === this.currentUserEmail) {
+      this.notifications.showError('Actually, there is nothing to change', 'Logic error');
+      return;
+    }
+    this.authService.changeUserEmail(emailFormValues.email).pipe(take(1)).subscribe(
+      () => {
+        this.notifications.showSuccess('Your email was successfully changed', 'Success');
+      },
+      (err: Error) => {
+        this.notifications.showError('Something went wrong', err.message);
+      }
+    )
+  }
 
   get currentUserEmail(): string {
     return this.authService.getUserData().email;
