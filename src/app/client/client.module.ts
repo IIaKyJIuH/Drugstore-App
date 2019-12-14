99af2f5dfa0dc75914018856ba6b930fe238dc3f
Pakulin Sergei <pakylin3@mail.ru>
2019-12-14 23:29:44 +0500

    +detailed medicine component

--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
 import { DataFormComponent } from './data-form/data-form.component';
+import { DetailedMedicineComponent } from './detailed-medicine/detailed-medicine.component';
 import { HomeComponent } from './home/home.component';
 import { StoreComponent } from './store/store.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
@@ -18,6 +19,7 @@ import { WrongPathComponent } from './wrong-path/wrong-path.component';
     WrongPathComponent,
     StoreComponent,
     UserProfileComponent,
+    DetailedMedicineComponent,
   ],
   imports: [
     SharedModule,
