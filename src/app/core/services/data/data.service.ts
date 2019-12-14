f86e65d018c971e1b106e811e39af8d89bd32087
Pakulin Sergei <pakylin3@mail.ru>
2019-12-14 23:24:51 +0500

    preparings for editing medicines

--- src/app/core/services/data/data.service.ts
+++ src/app/core/services/data/data.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs';
 import { AuthenticationService } from '../authentication/authentication.service';
 
 @Injectable({
@@ -7,12 +9,20 @@ import { AuthenticationService } from '../authentication/authentication.service'
 export class DataService {
 
   constructor(
-    private authService: AuthenticationService
+    private authService: AuthenticationService,
+    private database: AngularFireDatabase
   ) { }
 
   hasRole(appropriateRoles: string[]): boolean {
     return appropriateRoles.includes(this.authService.getUserData().role);
   }
 
+  getAllMedicines(): Observable<any> {
+    return this.database.object('/medicines').valueChanges();
+  }
+
+  editMedicine(pharmacy: number, medicine: number): void {
+    const item = this.database.list(`/medicines/list/${pharmacy}/${medicine}`);
+  }
   
 }
