58db968898518006bf1307c3ef8ea97d1e8309fc
Pakulin Sergei <pakylin3@mail.ru>
2019-12-14 23:17:01 +0500

    authentication changes

--- src/app/core/services/authentication/authentication.service.ts
+++ src/app/core/services/authentication/authentication.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { UserCredential } from '@firebase/auth-types';
-import { auth } from 'firebase';
+import { auth, User } from 'firebase';
 import { NgxPermissionsService, NgxRolesService } from 'ngx-permissions';
-import { BehaviorSubject, from, Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { from, Observable, of } from 'rxjs';
+import { map, switchMap, tap } from 'rxjs/operators';
 import { CredentialsModel } from '../models/credentials-model';
 import { UserModel } from '../models/user-model';
+import UserCredential = firebase.auth.UserCredential;
 
 /**
  * Service that authorizes user at FireBase.
@@ -19,14 +19,14 @@ export class AuthenticationService {
   /**
    * For getting the local storage state of authorized user email.
    */
-  public readonly USER_EMAIL = 'userEmail';
+  public readonly USER_EMAIL = 'USER_EMAIL';
 
   /**
    * For getting the locacl storage state of authorized user role.
    */
-  public readonly USER_ROLE = 'userRole';
+  public readonly USER_ROLE = 'USER_ROLE';
 
-  public authStatus = new BehaviorSubject<boolean>(false);
+  private authStatus: Observable<boolean>;
 
   /**
    * .сtor
@@ -35,15 +35,19 @@ export class AuthenticationService {
   constructor(
     private afAuth: AngularFireAuth,
     private ngxPermissions: NgxPermissionsService,
-    private ngxRoles: NgxRolesService,
-  ) { 
-    this.afAuth.auth.onAuthStateChanged((user) => {
-      if (user) {
-        this.authStatus.next(true);
-      } else {
-        this.authStatus.next(false);
-      }
-    })
+    private ngxRoles: NgxRolesService
+  ) {
+    this.authStatus = new Observable(observer => {
+      return this.afAuth.auth.onAuthStateChanged((user) => {
+        if (user) {
+          observer.next(true);
+          this.setUserData(user);
+        } else {
+          observer.next(false);
+          this.setUserData(user);
+        }
+      });
+    });
   }
 
   /**
@@ -52,11 +56,7 @@ export class AuthenticationService {
    * @returns firebase response user data flow.
    */
   public signIn(user: CredentialsModel): Observable<UserCredential> {
-    return from(this.afAuth.auth.signInWithEmailAndPassword(user.email, user.password)).pipe(
-      tap(userData => {
-        this.setUserData(userData);
-      }),
-    );
+    return from(this.afAuth.auth.signInWithEmailAndPassword(user.email, user.password));
   }
 
   /**
@@ -66,26 +66,27 @@ export class AuthenticationService {
    */
   public signUp(user: CredentialsModel): Observable<UserCredential> {
     return from(this.afAuth.auth.createUserWithEmailAndPassword(user.email, user.password)).pipe(
-      tap(userData => {
-        this.setUserData(userData);
-        userData.user.sendEmailVerification();
+      switchMap(userData => {
+        return of(userData.user.sendEmailVerification()).pipe(
+          map(() => userData)
+        )
       })
     );
   }
 
   /**
    * For setting user data to local storage.
-   * @param userData - user data from firebase response.
+   * @param user - user data from firebase response.
    */
-  private setUserData(userData: UserCredential): void {
-    const UID = userData.user.uid;
+  private setUserData(user: User): void {
+    const UID = user.uid;
     const permissions = ['watchStaffuser', 'editStaffuser', 'watchMedicines', 'bookMedicines', 'editMedicines', 'watchArchive', 'editArchive', 'doClientQueries', 'watchEmailuser', 'editEmailuser'];
     this.ngxPermissions.loadPermissions(permissions);
-    localStorage.setItem(this.USER_EMAIL, userData.user.email);
+    localStorage.setItem(this.USER_EMAIL, user.email);
     if (UID === 'boVXL3ic7bgn2mRWk1mSu5QpUFN2') {
       localStorage.setItem(this.USER_ROLE, 'ADMIN');
       this.ngxRoles.addRole('ADMIN', permissions);
-    } else if (UID === 'AIcnJji6nRP1sS7iOrErZe8LbPe2') {
+    } else if (UID === '1UepQikTzlMzzXVvK6tX8jAEGHI3') {
       localStorage.setItem(this.USER_ROLE, 'STAFF');
       this.ngxRoles.addRole('STAFF', ['watchArchive', 'doClientQueries']);
     } else {
@@ -130,7 +131,7 @@ export class AuthenticationService {
       this.getUserData().email,
       password
     );
-  
+
     return from(user.reauthenticateWithCredential(credential));
   }
 
@@ -154,11 +155,11 @@ export class AuthenticationService {
   }
 
   /**
-   * Checks if the user is logged in.
-   * @returns if the currentUser !== null - true, else - false.
+   * just returns auth object flow with auth state
+   * @returns if the currentUser !== null - isAuthenticated - true, else - false.
    */
-  get isAuthenticated(): Observable<boolean> {
-    return this.authStatus.asObservable();
+  getAuthStatus(): Observable<boolean> {
+    return this.authStatus;
   }
 
 }
