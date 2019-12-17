025057a5c85cc754dd15cad03e59aa4c264f9642
Pakulin Sergei <pakylin3@mail.ru>
2019-12-18 03:47:50 +0500

    too far with new component

---
+++ src/app/client/bookings/bookings.component.ts
@@ -0,0 +1,112 @@
+import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs';
+import { map, take, tap } from 'rxjs/operators';
+import { BookingsService } from 'src/app/core/services/data/bookings.service';
+import { ConfirmationDialogComponent } from 'src/app/shared/confirmation-dialog/confirmation-dialog.component';
+
+@Component({
+  selector: 'app-bookings',
+  templateUrl: './bookings.component.html',
+  styleUrls: ['./bookings.component.css']
+})
+export class BookingsComponent {
+
+  allBookings$: Observable<any>;
+
+  constructor(
+    private bookingsService: BookingsService,
+    private database: AngularFireDatabase,
+    private dialog: MatDialog
+  ) {
+    this.allBookings$ = this.getAllBookings();
+   }
+
+  getAllBookings(): Observable<any> {
+    const savedSpike = JSON.parse(localStorage.getItem('savedSpike'));
+    return this.bookingsService.getAllBookings()
+      .pipe(
+        map(bookingsArr => {
+          for(const item of bookingsArr) {
+            Object.assign(item, {
+              isReady: this.tryGetLocalStorageValue(item)
+            });
+          }
+          return bookingsArr;
+        }),
+        tap(console.log)
+      );
+  }
+
+  setToPrepared(booking): void {
+    booking.isReady = true;
+    this.saveToLocalStorage(booking);
+    // TODO: send email notification
+  }
+
+  private saveToLocalStorage(booking): void {
+    const savedSpike = localStorage.getItem('savedSpike') ? JSON.parse(localStorage.getItem('savedSpike')) : [];
+    let found = false;
+    for(const each of savedSpike) {
+      if (JSON.stringify(each) === JSON.stringify(booking)) {
+        found = true;
+        break;
+      }
+    }
+    if (!found) {
+      savedSpike.push(booking);
+      localStorage.setItem('savedSpike', JSON.stringify(savedSpike));
+    }
+  }
+
+  private tryGetLocalStorageValue(item): boolean {
+    const savedSpike = localStorage.getItem('savedSpike') ? JSON.parse(localStorage.getItem('savedSpike')) : [];
+    for(const each of savedSpike) {
+      const reducedObj = Object.assign({}, {
+        email: each.email,
+        items: each.items
+      });
+      if (JSON.stringify(item) === JSON.stringify(reducedObj)) {
+        return each.isReady;
+      }
+    }
+    return false;
+  }
+
+  setToSuccessfulTransaction(booking): void {
+
+  }
+
+  setToFailedTransaction(booking): void {
+    
+  }
+
+  getMyBookings(): Observable<any> {
+    return this.bookingsService.getMyBookings();
+  }
+
+  unBookTransaction(transaction): void {
+    this.openConfirmationDialog()
+      .pipe(take(1))
+      .subscribe(
+        (isConfirmed) => {
+          if (isConfirmed) {
+            this.bookingsService.unBookTransactionFrom(transaction);
+          }
+        }
+      );
+  }
+
+  private openConfirmationDialog(): Observable<any> {
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      width: '50vw',
+      data: {
+        message: 'Your booking will be cancelled'
+      }
+    });
+
+    return dialogRef.afterClosed();
+  }
+
+}
--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
+import { BookingsComponent } from './bookings/bookings.component';
 import { DataFormComponent } from './data-form/data-form.component';
 import { DetailedMedicineComponent } from './detailed-medicine/detailed-medicine.component';
 import { HomeComponent } from './home/home.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { StoreComponent } from './store/store.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { WrongPathComponent } from './wrong-path/wrong-path.component';
-import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 
 /**
@@ -22,6 +23,7 @@ import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
     UserProfileComponent,
     DetailedMedicineComponent,
     ShoppingCartComponent,
+    BookingsComponent,
   ],
   imports: [
     SharedModule,
