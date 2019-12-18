4453d13390c7d39f3124ffbb95ab3621d08ab8c7
Pakulin Sergei <pakylin3@mail.ru>
2019-12-19 00:16:06 +0500

    names fixes

---
+++ src/app/admin/staff-statistics/staff-statistics.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-staff-statistics',
+  templateUrl: './staff-statistics.component.html',
+  styleUrls: ['./staff-statistics.component.css']
+})
+export class StaffStatisticsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/admin/users-statistics/users-statistics.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-users-statistics',
+  templateUrl: './users-statistics.component.html',
+  styleUrls: ['./users-statistics.component.css']
+})
+export class UsersStatisticsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
