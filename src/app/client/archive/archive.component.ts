8be8ccd780481feaabcf97a9e42211a433bec2ad
Pakulin Sergei <pakylin3@mail.ru>
2019-12-18 22:33:08 +0500

    initial archive data

--- src/app/client/archive/archive.component.ts
+++ src/app/client/archive/archive.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { ArchiveService } from 'src/app/core/services/data/archive.service';
 
 @Component({
   selector: 'app-archive',
   templateUrl: './archive.component.html',
   styleUrls: ['./archive.component.css']
 })
-export class ArchiveComponent implements OnInit {
+export class ArchiveComponent {
 
-  constructor() { }
+  allTransactions$: Observable<any>;
 
-  ngOnInit() {
+  constructor(
+    private archiveService: ArchiveService
+  ) { 
+    this.allTransactions$ = archiveService.getAllTransactions()
+      .pipe(tap(console.log));
   }
 
 }
