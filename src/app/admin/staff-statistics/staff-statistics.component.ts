d66111ce1c7c15b0bb8968fe63acb08ef772c84c
Pakulin Sergei <pakylin3@mail.ru>
2019-12-19 02:29:10 +0500

    staff component almost done

--- src/app/admin/staff-statistics/staff-statistics.component.ts
+++ src/app/admin/staff-statistics/staff-statistics.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { StatisticsService } from 'src/app/core/services/data/statistics.service';
 
 @Component({
@@ -13,8 +14,16 @@ export class StaffStatisticsComponent {
 
   constructor(
     private statisticsService: StatisticsService
-  ) { 
-    this.staffStatistics$ = statisticsService.getStaffStatistics();
+  ) {
+    this.staffStatistics$ = statisticsService.getStaffStatistics()
+      .pipe(
+        map(staffList => {
+          const filtered = staffList
+            .sort((a, b) => b.purchased - a.purchased)
+            .slice(0, 1);
+          return filtered;
+        })
+      );
   }
 
 }
