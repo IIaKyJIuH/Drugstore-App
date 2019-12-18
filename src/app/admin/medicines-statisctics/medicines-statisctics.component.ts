c2c377ada8debca4615625d3a37f52fc60dbcf42
Pakulin Sergei <pakylin3@mail.ru>
2019-12-19 00:02:19 +0500

    +medicines-statistics page

---
+++ src/app/admin/medicines-statisctics/medicines-statisctics.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-medicines-statisctics',
+  templateUrl: './medicines-statisctics.component.html',
+  styleUrls: ['./medicines-statisctics.component.css']
+})
+export class MedicinesStatiscticsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
