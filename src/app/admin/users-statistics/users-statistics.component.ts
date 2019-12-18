436c02a3880e734d9e78f5b24483dbb212159f50
Pakulin Sergei <pakylin3@mail.ru>
2019-12-19 02:29:25 +0500

    users statistics almost done

--- src/app/admin/users-statistics/users-statistics.component.ts
+++ src/app/admin/users-statistics/users-statistics.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { StatisticsService } from 'src/app/core/services/data/statistics.service';
 
 @Component({
@@ -14,7 +15,15 @@ export class UsersStatisticsComponent {
   constructor(
     private statisticsService: StatisticsService
   ) { 
-    this.usersStatistics$ = statisticsService.getUsersStatistics();
+    this.usersStatistics$ = statisticsService.getUsersStatistics()
+      .pipe(
+        map(users => {
+          const filtered = staffList
+            .sort((a, b) => b.purchased - a.purchased)
+            .slice(0, 1);
+          return filtered;
+        })
+      );
   }
 
 }
