70327d4f923c9de478ba0acf93ee3db3e087ff2f
Pakulin Sergei <pakylin3@mail.ru>
2019-12-18 11:46:42 +0500

    +statistics interface

--- src/app/core/services/data/statistics.service.ts
+++ src/app/core/services/data/statistics.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { NEVER, Observable } from 'rxjs';
+import { AuthenticationService } from '../authentication/authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StatisticsService {
 
-  constructor() { }
+  staffEmail: string;
+
+  constructor(
+    private database: AngularFireDatabase,
+    private authService: AuthenticationService
+  ) { 
+    this.staffEmail = this.authService.getUserData().email;
+  }
+
+  getMedicinesStatistics(): Observable<any> {
+    return NEVER;
+  }
+
+  getStaffStatistics(): Observable<any> {
+    return NEVER;
+  }
+
+  getUsersStatistics(): Observable<any> {
+    return NEVER;
+  }
+
+  writeSuccessfulTransaction(element): void {
+    
+  }
+
+  writeUnBookedTransaction(element): void {
+
+  }
+
+  writeFailedTransaction(element): void {
+
+  }
 }
