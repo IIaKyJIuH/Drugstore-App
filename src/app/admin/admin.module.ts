e2fcd81141695074b152d4e31cd947233c4c9a18
Pakulin Sergei <pakylin3@mail.ru>
2019-12-19 03:47:46 +0500

    admin imports

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,15 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule, MatTabsModule } from '@angular/material';
 import { MaterialModule } from '../material.module';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { MedicinesStatisticsComponent } from './medicines-statistics/medicines-statistics.component';
+import { StaffEditComponent } from './staff-edit/staff-edit.component';
 import { StaffStatisticsComponent } from './staff-statistics/staff-statistics.component';
 import { UsersStatisticsComponent } from './users-statistics/users-statistics.component';
-import { StaffEditComponent } from './staff-edit/staff-edit.component';
 
 
 ***REMOVED****
@@ -21,6 +21,7 @@ import { StaffEditComponent } from './staff-edit/staff-edit.component';
     CommonModule,
     AdminRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     MaterialModule,
     MatTabsModule,
     MatCardModule,
