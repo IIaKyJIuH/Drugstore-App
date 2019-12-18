3c9062153cfed3dbee1221355619f6dd27b21c09
Pakulin Sergei <pakylin3@mail.ru>
2019-12-18 12:42:49 +0500

    done with archive service

--- src/app/core/services/data/archive.service.ts
+++ src/app/core/services/data/archive.service.ts
@@ -1,24 +1,55 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { AuthenticationService } from '../authentication/authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArchiveService {
 
+  currentUser: any;
+
   constructor(
-    private database: AngularFireDatabase
-  ) { }
+    private database: AngularFireDatabase,
+    private authService: AuthenticationService
+  ) {
+    this.currentUser = this.authService.getUserData();
+   }
 
   writeUnBookedTransaction(transaction): void {
-
+    const currentDate = new Date();
+    const currentTime = `${currentDate.getFullYear()}-${currentDate.getMonth()+1}-${currentDate.getDate()}, ${currentDate.getHours()}:${currentDate.getMinutes()}`;
+    this.database.list('/archive/transactions/').push({
+      date: currentTime,
+      purchases: transaction.items.length,
+      staffEmail: this.currentUser.email,
+      userEmail: transaction.email,
+      status: 'unbooked'
+    });
   }
 
   writeSuccessfulTransaction(transaction): void {
-
+    const currentDate = new Date();
+    const currentTime = `${currentDate.getFullYear()}-${currentDate.getMonth()+1}-${currentDate.getDate()}, ${currentDate.getHours()}:${currentDate.getMinutes()}`;
+    this.database.list('/archive/transactions/').push({
+      date: currentTime,
+      purchases: transaction.items.length,
+      staffEmail: this.currentUser.email,
+      userEmail: transaction.email,
+      status: 'success'
+    });
   }
 
   writeFailedTransaction(transaction): void {
-    
+    const currentDate = new Date();
+    const currentTime = `${currentDate.getFullYear()}-${currentDate.getMonth()+1}-${currentDate.getDate()}, ${currentDate.getHours()}:${currentDate.getMinutes()}`;
+    this.database.list('/archive/transactions/').push({
+      date: currentTime,
+      purchases: transaction.items.length,
+      staffEmail: this.currentUser.email,
+      userEmail: transaction.email,
+      status: 'failure'
+    });
   }
+
 }
