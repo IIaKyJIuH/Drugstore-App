dfb801cd84a1564462af75acafa5f6822dbe7237
Pakulin Sergei <pakylin3@mail.ru>
2019-12-18 18:53:32 +0500

    done with statistics service

--- src/app/core/services/data/statistics.service.ts
+++ src/app/core/services/data/statistics.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { NEVER, Observable } from 'rxjs';
+import { take, tap } from 'rxjs/operators';
 import { AuthenticationService } from '../authentication/authentication.service';
 
 @Injectable({
@@ -8,14 +9,10 @@ import { AuthenticationService } from '../authentication/authentication.service'
 })
 export class StatisticsService {
 
-  currentUser: any;
-
   constructor(
     private database: AngularFireDatabase,
     private authService: AuthenticationService
-  ) { 
-    this.currentUser = this.authService.getUserData();
-  }
+  ) {}
 
   getMedicinesStatistics(): Observable<any> {
     return NEVER;
@@ -30,14 +27,120 @@ export class StatisticsService {
   }
 
   writeSuccessfulTransaction(transaction): void {
-    
+    const userEmail = transaction.email;
+    this.writeMedicines(transaction.items);
+    this.writeSuccessfulUser(transaction);
+    this.writeStaff(transaction);
   }
 
   writeCancelledBooking(booking): void {
-
+    this.writeCancelledUser(booking);
   }
 
   writeFailedTransaction(transaction): void {
 
+  }
+
+  private writeMedicines(medicines): void {
+    this.database.object('/statistics/medicines/').valueChanges()
+    .pipe(
+      take(1),
+      tap(records => {
+        for (const item of medicines) {
+          let isFound = false;
+          for (const recordKey of Object.keys(records)) {
+            if (records[recordKey].term === item.term) {
+              isFound = true;
+              this.database.object(`/statistics/medicines/${recordKey}/purchased`).set(records[recordKey].purchased + item.count);
+            }
+          }
+          if (!isFound) {
+            this.database.list('/statistics/medicines/').push(
+              Object.assign({}, {
+                purchased: item.count,
+                term: item.term
+              })
+            );
+          }
+        }
+      })
+    ).subscribe();
+  }
+
+  private writeStaff(transaction): void {
+    const staffEmail = this.authService.getUserData().email;
+    const itemsCount = transaction.items.reduce((temp, {count}) => temp + count, 0);
+    this.database.object('/statistics/staff/').valueChanges()
+      .pipe(
+        take(1),
+        tap(records => {
+          let isFound = false;
+          for (const recordKey of Object.keys(records)) {
+            if (records[recordKey].email === staffEmail) {
+              isFound = true;
+              this.database.object(`/statistics/staff/${recordKey}/purchasedItems`).set(records[recordKey].purchasedItems + itemsCount);
+            }
+          }
+          if (!isFound) {
+            this.database.list(`/statistics/staff/`).push(
+              Object.assign({}, {
+                email: staffEmail,
+                purchasedItems: itemsCount
+              })
+            );
+          }
+        })
+      ).subscribe();
+  }
+
+  private writeSuccessfulUser(transaction): void {
+    const itemsCount = transaction.items.reduce((temp, {count}) => temp + count, 0);
+    this.database.object('/statistics/users/').valueChanges()
+      .pipe(
+        take(1),
+        tap(records => {
+          let isFound = false;
+          for (const recordKey of Object.keys(records)) {
+            if (records[recordKey].email === transaction.email) {
+              isFound = true;
+              this.database.object(`/statistics/users/${recordKey}/purchasedItems`).set(records[recordKey].purchasedItems + itemsCount);
+            }
+          }
+          if (!isFound) {
+            this.database.list(`/statistics/users/`).push(
+              Object.assign({}, {
+                cancelledBookings: 0,
+                email: transaction.email,
+                purchasedItems: itemsCount
+              })
+            );
+          }
+        })
+      ).subscribe();
+  }
+
+  private writeCancelledUser(transaction): void {
+    this.database.object('/statistics/users/').valueChanges()
+      .pipe(
+        take(1),
+        tap(records => {
+          let isFound = false;
+          for (const recordKey of Object.keys(records)) {
+            if (records[recordKey].email === transaction.email) {
+              isFound = true;
+              this.database.object(`/statistics/users/${recordKey}/cancelledBookings`).set(records[recordKey].cancelledBookings + 1);
+            }
+          }
+          if (!isFound) {
+            this.database.list(`/statistics/users/`).push(
+              Object.assign({}, {
+                cancelledBookings: 1,
+                email: transaction.email,
+                purchasedItems: 0
+              })
+            );
+          }
+        })
+      ).subscribe();
   }
 }
