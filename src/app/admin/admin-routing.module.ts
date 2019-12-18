c9c2f4ef593f9e5829bbdcfe477e254d14087431
Pakulin Sergei <pakylin3@mail.ru>
2019-12-18 23:44:12 +0500

    optimized admin routes

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-
+import { AdminComponent } from './admin.component';
 
 const routes: Routes = [
-		{},
+  {
+    path: '',
+    pathMatch: 'full',
+    component: AdminComponent
+  }
 ];
 
 ***REMOVED****
