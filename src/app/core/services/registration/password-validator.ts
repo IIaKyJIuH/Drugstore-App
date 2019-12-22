c8a68cd9ef3a52ba44aa08477b4abb8b3063c809
Pakulin Sergei <pakylin3@mail.ru>
2019-12-22 14:30:37 +0500

    +comments

--- src/app/core/services/registration/password-validator.ts
+++ src/app/core/services/registration/password-validator.ts
@@ -4,6 +4,7 @@ import { FormGroup } from '@angular/forms';
  * Custom validator for angular forms password.
  */
 export class PasswordValidator {
+
   /**
    * Checks password equality.
    * @param group - includes password and repeatedPassword.
@@ -12,7 +13,7 @@ export class PasswordValidator {
   public static areEqual(group: FormGroup): {[key: string]: boolean} {
     const password = group.value.password;
     const confirmPassword = group.value.confirmPassword;
-    if (!group.controls.confirmPassword.validator) { return null; }
+    if (!group.controls.confirmPassword.validator) { return null; } // Needed because of the validator used on the same page with sign-in and sign-up
     return password.localeCompare(confirmPassword) === 0
       ? null
       : { nomatch: true };
