1d3e22886da13e96638bcc983030c0fe620618b8
Pakulin Sergei <pakylin3@mail.ru>
2019-12-22 14:29:01 +0500

    +comments

--- src/app/core/services/notification/notification.service.ts
+++ src/app/core/services/notification/notification.service.ts
@@ -6,17 +6,38 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class NotificationService {
 
-  constructor(private toastr: ToastrService) { }
+ ***REMOVED*****REMOVED****
+  ***REMOVED*** .ctor
+  ***REMOVED*** @param toaster - out module, that is used as toasting service.
+  ***REMOVED***/
+  constructor(
+    private toaster: ToastrService
+  ) { }
 
+ ***REMOVED*****REMOVED****
+  ***REMOVED*** With green background.
+  ***REMOVED*** @param message - will be displayed as normal text.
+  ***REMOVED*** @param title - will be displayed in bold at the top.
+  ***REMOVED***/
   showSuccess(message, title): void {
-      this.toastr.success(message, title);
+      this.toaster.success(message, title);
   }
 
+ ***REMOVED*****REMOVED****
+  ***REMOVED*** With red background.
+  ***REMOVED*** @param message - will be displayed as normal text.
+  ***REMOVED*** @param title - will be displayed in bold at the top.
+  ***REMOVED***/
   showError(message, title): void {
-    this.toastr.error(message, title);
+    this.toaster.error(message, title);
   }
 
+ ***REMOVED*****REMOVED****
+  ***REMOVED*** With yellow background.
+  ***REMOVED*** @param message - will be displayed as normal text.
+  ***REMOVED*** @param title - will be displayed in bold at the top.
+  ***REMOVED***/
   showWarning(message, title): void {
-    this.toastr.warning(message, title);
+    this.toaster.warning(message, title);
   }
 }
