f5c0c065743edf50054d765e29c177984dc42beb
Pakulin Sergei <pakylin3@mail.ru>
2020-01-11 01:38:04 +0500

    actualized with models

--- src/app/client/archive/archive.component.ts
+++ src/app/client/archive/archive.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { ArchiveService } from 'src/app/core/services/data/archive.service';
+import { ArchiveModel } from '../../core/services/models/archive/archive-model';
+import { BookingModel } from '../../core/services/models/bookings/booking-model';
 
 @Component({
   selector: 'app-archive',
@@ -10,14 +12,14 @@ import { ArchiveService } from 'src/app/core/services/data/archive.service';
 })
 export class ArchiveComponent {
 
-  allTransactions$: Observable<any>;
+  allTransactions$: Observable<ArchiveModel[]>;
 
   constructor(
     private archiveService: ArchiveService
   ) {
     this.allTransactions$ = archiveService.getAllTransactions()
       .pipe(
-        map(transactions => {
+        map((transactions: ArchiveModel[]) => {
           return transactions.sort((a, b) => b.date.localeCompare(a.date));
         })
       );
