4e3cb95ea305e0c0b7d93314c2e22b4b0d511e2d
Pakulin Sergei <pakylin3@mail.ru>
2020-01-11 01:29:30 +0500

    linting

--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
+import { ArchiveComponent } from './archive/archive.component';
 import { BookingsComponent } from './bookings/bookings.component';
 import { DataFormComponent } from './data-form/data-form.component';
 import { DetailedMedicineComponent } from './detailed-medicine/detailed-medicine.component';
@@ -9,7 +10,6 @@ import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { StoreComponent } from './store/store.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { WrongPathComponent } from './wrong-path/wrong-path.component';
-import { ArchiveComponent } from './archive/archive.component';
 
 
 /**
