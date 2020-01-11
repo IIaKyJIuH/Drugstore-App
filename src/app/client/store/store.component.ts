7f0d8d541b89dbaa49cd9eec69f9aabb2ab27b29
Pakulin Sergei <pakylin3@mail.ru>
2020-01-11 16:48:35 +0500

    +comments

--- src/app/client/store/store.component.ts
+++ src/app/client/store/store.component.ts
@@ -18,15 +18,25 @@ import { MedicineModel } from '../../core/services/models/medicines/medicine-mod
 })
 export class StoreComponent implements AfterViewInit {
 
+***REMOVED*****REMOVED*****REMOVED****
+ ***REMOVED*****REMOVED*** All medicines from db.
+ ***REMOVED*****REMOVED***/
   medicines$: Observable<MatTableDataSource<any>>;
 
-  displayedColumns: string[] = ['pharmacy', 'name', 'amount', 'controls'];
-  pharmaciesList: string[] = [];
+  displayedColumns: string[] = ['pharmacy', 'name', 'amount', 'controls']; // For mat-table.
+  pharmaciesList: string[] = []; // for admin table.
 
-  @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
-  @ViewChild(MatSort, { static: false }) sort: MatSort;
-  @ViewChild('filter', { static: false}) filter: ElementRef;
+  @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator; // For pagination of the table.
+  @ViewChild(MatSort, { static: false }) sort: MatSort; // For sorting specific columns of the table.
+  @ViewChild('filter', { static: false}) filter: ElementRef; // For filtering data by given key.
 
+***REMOVED*****REMOVED*****REMOVED****
+ ***REMOVED*****REMOVED*** .ctor
+ ***REMOVED*****REMOVED*** @param router - for redirecting user.
+ ***REMOVED*****REMOVED*** @param dataService - for getting data from db.
+ ***REMOVED*****REMOVED*** @param shoppingCart - for adding/removing data to shopping cart.
+ ***REMOVED*****REMOVED*** @param authService - for getting current auth data.
+ ***REMOVED*****REMOVED***/
   constructor(
     private router: Router,
     private dataService: DataService,
@@ -34,6 +44,9 @@ export class StoreComponent implements AfterViewInit {
     private authService: AuthenticationService,
   ) {}
 
+***REMOVED*****REMOVED*****REMOVED****
+ ***REMOVED*****REMOVED*** Gets all medicines from db.
+ ***REMOVED*****REMOVED***/
   getAllMedicines(): Observable<MatTableDataSource<any>> {
     return this.dataService.getAllMedicines().pipe(
       map((medicines: MedicineModel[]) => {
@@ -51,18 +64,34 @@ export class StoreComponent implements AfterViewInit {
     );
   }
 
+***REMOVED*****REMOVED*****REMOVED****
+ ***REMOVED*****REMOVED*** Adds/increases given element to shopping cart.
+ ***REMOVED*****REMOVED*** @param element - to be added/increased.
+ ***REMOVED*****REMOVED***/
   addToCart(element): void {
     this.shoppingCart.addItem(element);
   }
 
+***REMOVED*****REMOVED*****REMOVED****
+ ***REMOVED*****REMOVED*** Removes/subtracts given element from shopping cart.
+ ***REMOVED*****REMOVED*** @param element - to be removed/subtracted.
+ ***REMOVED*****REMOVED***/
   subFromCart(element): void {
     this.shoppingCart.minusItem(element);
   }
 
+***REMOVED*****REMOVED*****REMOVED****
+ ***REMOVED*****REMOVED*** Checks if the given element amount in the cart is less than in db.
+ ***REMOVED*****REMOVED*** @param element - to be checked on.
+ ***REMOVED*****REMOVED***/
   isEnough(element): boolean {
     return this.shoppingCart.isEnough(element);
   }
 
+***REMOVED*****REMOVED*****REMOVED****
+ ***REMOVED*****REMOVED*** Checks if the given element is in the cart right now.
+ ***REMOVED*****REMOVED*** @param element - to be checked.
+ ***REMOVED*****REMOVED***/
   isInCart(element): boolean {
     return this.shoppingCart.isInCart(element);
   }
@@ -71,6 +100,11 @@ export class StoreComponent implements AfterViewInit {
     this.router.navigate(['/store', row.pharmacy, row.id]);
   }
 
+***REMOVED*****REMOVED*****REMOVED****
+ ***REMOVED*****REMOVED*** ADMIN! Moves given element to another pharmacy of the db.
+ ***REMOVED*****REMOVED*** @param element - to be moved.
+ ***REMOVED*****REMOVED*** @param option - new place for element.
+ ***REMOVED*****REMOVED***/
   moveToPharmacy(element, option: string): void {
     if (element.pharmacy === option) {
       alert('select appropriate option');
@@ -82,6 +116,10 @@ export class StoreComponent implements AfterViewInit {
     this.dataService.moveToAnotherPharmacy(option, element);
   }
 
+***REMOVED*****REMOVED*****REMOVED****
+ ***REMOVED*****REMOVED*** ADMIN! Deletes given element from db.
+ ***REMOVED*****REMOVED*** @param element - to be removed.
+ ***REMOVED*****REMOVED***/
   deleteFromDb(element): void {
     this.dataService.deleteMedicineFromDb(element);
   }
