afd450674eea267bf7c13133c44bc8e5210023b1
Pakulin Sergei <pakylin3@mail.ru>
2020-01-11 16:38:58 +0500

    +comments

--- src/app/client/shopping-cart/shopping-cart.component.ts
+++ src/app/client/shopping-cart/shopping-cart.component.ts
@@ -14,19 +14,36 @@ import { ConfirmationDialogComponent } from 'src/app/shared/confirmation-dialog/
 })
 export class ShoppingCartComponent {
 
+  /**
+   * Items selected by user for booking.
+   */
   myItems$: Observable<any>;
 
+  /**
+   * Columns for mat-table.
+   */
   displayedColumns = ['Name', 'Count', 'Pharmacy', 'Controls'];
 
+  /**
+   * .ctor
+   * @param dialog - for confirmation of the booking operation.
+   * @param cartService - for dealing with cart items.
+   * @param bookingsService - for booking specific items.
+   * @param notifications - for getting info about operations.
+   */
   constructor(
+    private dialog: MatDialog,
     private cartService: ShoppingCartService,
-    private notifications: NotificationService,
     private bookingsService: BookingsService,
-    private dialog: MatDialog
+    private notifications: NotificationService,
   ) {
     this.myItems$ = this.cartService.getCurrentCart();
   }
 
+  /**
+   * Books selected items after confirmation.
+   * @param items - to be booked.
+   */
   bookItems(items): void {
     this.openConfirmationDialog()
       .pipe(take(1))
@@ -41,6 +58,9 @@ export class ShoppingCartComponent {
       );
   }
 
+  /**
+   * For confirmation of the booking.
+   */
   private openConfirmationDialog(): Observable<any> {
     const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
       width: '400px',
@@ -52,18 +72,34 @@ export class ShoppingCartComponent {
     return dialogRef.afterClosed();
   }
 
+  /**
+   * Removes all selected items from shopping-cart.
+   * @param items - to be deleted.
+   */
   removeAll(items): void {
     this.cartService.removeItems(items);
   }
 
+  /**
+   * Adds or increases cart with the given element.
+   * @param element - to be add/increased.
+   */
   addToCart(element): void {
     this.cartService.addItem(element);
   }
 
+  /**
+   * Removes or subtracts the given element from cart.
+   * @param element - to be removed/subtracted.
+   */
   subFromCart(element): void {
     this.cartService.minusItem(element);
   }
 
+  /**
+   * For checking if the given element is in cart now.
+   * @param element - to be checked.
+   */
   isInCart(element): boolean {
     return this.cartService.isInCart(element);
   }
